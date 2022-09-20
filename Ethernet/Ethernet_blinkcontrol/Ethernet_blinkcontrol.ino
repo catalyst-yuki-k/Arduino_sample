c00500440af0e4b692359946d2c6ad387d0b930e
catalyst-yuki-k <catalyst-yuki_k@hotmail.com>
2022-09-20 21:33:03 +0900

    完成版

--- Ethernet/Ethernet_blinkcontrol/Ethernet_blinkcontrol.ino
+++ Ethernet/Ethernet_blinkcontrol/Ethernet_blinkcontrol.ino
@@ -12,30 +12,23 @@ IPAddress ip(192, 168, 1, 177);
 // （HTTPの場合は80番がデフォルト）
 EthernetServer server(80);
 
+//LEDの出力ピンの定義
 int ledPin = 3;
+//LED状態の変数
+int led_value;
 
 void setup() {
-    // Ethernet接続を開始し、サーバーを起動します。:
+    // Ethernet接続を開始し、サーバーを起動します
     Ethernet.begin(mac, ip);
 
     // サーバを起動する
     server.begin();
 
+    //LEDの初期化
     pinMode(ledPin,OUTPUT);
     digitalWrite(ledPin,LOW);
 
-    digitalWrite(ledPin, HIGH);   // LEDをオン
-    delay(1000);                  // 1秒待つ
-    digitalWrite(ledPin, LOW);    // LEDをオフ
-    delay(1000);
-    digitalWrite(ledPin, HIGH);   // LEDをオン
-    delay(1000);                  // 1秒待つ
-    digitalWrite(ledPin, LOW);    // LEDをオフ
-    delay(1000);
-    digitalWrite(ledPin, HIGH);   // LEDをオン
-    delay(1000);                  // 1秒待つ
-    digitalWrite(ledPin, LOW);    // LEDをオフ
-    delay(1000);
+    led_value = 0;
 }
 
 void loop() {
@@ -53,19 +46,25 @@ void loop() {
 
     // Read the first line of the request
     String request = client.readStringUntil('\r');
+
+    //バッファ内のすべての送信文字が送信されるまで待機
     client.flush();
 
-    // Match the request
-    int value = LOW;
-    if (request.indexOf("/LED=ON"))  {
+    // ページアドレス後方の「=」が何文字目であるか取得
+    int led_value_index = (int)request.indexOf("=");
+    // 「=」の後に続く文字(ONは1、OFFは0)を取得
+    String led_value_str = request.substring(led_value_index+1,led_value_index+2);
+
+    if (led_value_str.equals("1"))  {//「=」の後に続く文字が1の場合はONに切り替える
         digitalWrite(ledPin, HIGH);
-        value = HIGH;
-    }
-    if (request.indexOf("/LED=OFF"))  {
+        led_value = 1;//フラグを切り替える
+    }else if(led_value_str.equals("0")){//「=」の後に続く文字が0の場合はOFFに切り替える
         digitalWrite(ledPin, LOW);
-        value = LOW;
+        led_value = 0;//フラグを切り替える
+    }else{//どちらでもない場合は何もしない
+        ;
     }
- 
+
     // Return the response
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
@@ -74,13 +73,15 @@ void loop() {
     client.println();
     client.println("<!DOCTYPE HTML>");
     client.println("<html>");
-    if(value == HIGH) {
-        client.print("On");
-    } else {
-        client.print("Off");
+    if(led_value==1) {//1の場合はページの表示を「LED is ON」に切り替える
+        client.print("LED is ON<br/>");
+    } else {//0の場合はページの表示を「LED is OFF」に切り替える
+        client.print("LED is OFF<br/>");
     }
-    client.println("<a href=\"/LED=ON\"\"><button>LED ON</button></a>");
-    client.println("<a href=\"/LED=OFF\"\"><button>LED OFF</button></a><br />");  
+     client.println("<form ACTION='/' method=get>");
+    client.println("<button type='submit' name='LED' value='1'>ON</button>");
+    client.println("<button type='submit' name='LED' value='0'>OFF</button>");
+    client.println("</form>");
     client.println("</html>");
 
     delay(1);
