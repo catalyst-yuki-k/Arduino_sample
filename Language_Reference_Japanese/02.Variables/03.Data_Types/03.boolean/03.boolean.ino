4bf9079f27e17671f5e866f5c4d99ea460513629
catalyst-yuki-k <catalyst-yuki_k@hotmail.com>
2022-10-11 22:53:16 +0900

    追記

---
+++ Language_Reference_Japanese/02.Variables/03.Data_Types/03.boolean/03.boolean.ino
@@ -0,0 +1,3 @@
+// Description
+// boolean is a non-standard type alias for bool defined by Arduino. 
+// It’s recommended to instead use the standard type bool, which is identical.
---
+++ Language_Reference_Japanese/02.Variables/03.Data_Types/04.byte/04.byte.ino
@@ -0,0 +1,7 @@
+// Description
+// A byte stores an 8-bit unsigned number, from 0 to 255.
+// Syntax
+// byte var = val;
+// Parameters
+// var: variable name.
+// val: the value to assign to that variable.
