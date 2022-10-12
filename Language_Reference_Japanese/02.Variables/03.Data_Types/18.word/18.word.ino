14ff9670f1d6b23b3d66d011d4be3fbe9cdb83b3
catalyst-yuki-k <catalyst-yuki_k@hotmail.com>
2022-10-12 16:54:05 +0900

    修正

--- Language_Reference_Japanese/02.Variables/03.Data_Types/18.word/18.word.ino
+++ Language_Reference_Japanese/02.Variables/03.Data_Types/18.word/18.word.ino
@@ -1,17 +1,12 @@
 // Description
-// Long variables are extended size variables for number storage, 
-// and store 32 bits (4 bytes), from -2,147,483,648 to 2,147,483,647.
-// 
-// If doing math with integers at least one of the values must be of type long, 
-// either an integer constant followed by an L or a variable of type long, 
-// forcing it to be a long. See the Integer Constants page for details.
+// A word can store an unsigned number of at least 16 bits (from 0 to 65535).
 // 
 // Syntax
-// long var = val;
+// word var = val;
 // 
 // Parameters
 // var: variable name.
-// val: the value assigned to the variable.
+// val: the value to assign to that variable.
 // 
 // Example Code
-// long speedOfLight_km_s = 300000L;  // see the Integer Constants page for explanation of the 'L'
+word w = 10000;
