1a00d3f37f17b7c382130d64083a2df46b7b54b3
catalyst-yuki-k <catalyst-yuki_k@hotmail.com>
2022-11-21 22:51:53 +0900

    追記

--- Language_Reference_Japanese/01.Functions/04.Advanced _Input_Output/01.noTone()/01.noTone().ino
+++ Language_Reference_Japanese/01.Functions/04.Advanced _Input_Output/01.noTone()/01.noTone().ino
@@ -1,1 +1,12 @@
-noTone(pin);
+int BuzzPin = 11; // 変数"BuzzPin"にブザーを接続している11番ピンを割り当てる。
+int val = 0;      // 読み込んだ値を保存するための変数"val"を0で初期化する。
+
+void setup() {
+    pinMode(BuzzPin, OUTPUT); // 変数"BuzzPin"を出力として設定する。
+} 
+void loop() { 
+    tone(BuzzPin,440); //"BuzzPin"から440Hzを出力する
+    delay(1000);       //1秒間まつ
+    noTone(BuzzPin);   //音止める
+    delay(1000);       //1秒間まつ
+}
--- Language_Reference_Japanese/01.Functions/04.Advanced _Input_Output/06.tone()/06.tone().ino
+++ Language_Reference_Japanese/01.Functions/04.Advanced _Input_Output/06.tone()/06.tone().ino
@@ -1,2 +1,15 @@
-tone(pin, frequency);
-tone(pin, frequency, duration);
+int BuzzPin = 11; // 変数"BuzzPin"にブザーを接続している11番ピンを割り当てる。
+int val = 0;      // 読み込んだ値を保存するための変数"val"を0で初期化する。
+
+void setup() {
+    pinMode(BuzzPin, OUTPUT); // 変数"BuzzPin"を出力として設定する。
+} 
+void loop() { 
+    tone(BuzzPin,440);      //"BuzzPin"から440Hzを出力する
+    delay(1000);            //1秒間まつ
+    noTone(BuzzPin);        //音止める
+    delay(1000);            //1秒間まつ
+    tone(BuzzPin,440,1000); //"BuzzPin"から440Hzを1000ms間出力する
+    noTone(BuzzPin);        //音止める
+    delay(1000);            //1秒間まつ
+}
